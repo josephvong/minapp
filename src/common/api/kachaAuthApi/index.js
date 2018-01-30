e89ebabaed0c85372e7632a25003380d9f6114b9
josephvong <m15914375662@163.com>
2018-01-30 20:35:08 +0800

    20180130

--- src/common/api/kachaAuthApi/index.js
+++ src/common/api/kachaAuthApi/index.js
@@ -1,21 +1,37 @@
-import wepy from 'wepy'
-export function kachaAuthApi(loginInfo,userData){
-  let reqdata = `{"v":"1.0.0","client":"wx","command":"1003","user":{"code":${loginInfo.code},"nickName":${userData.userInfo.nickName},"headurl":${userData.userInfo.avatarUrl},"sex":${userData.userInfo.gender},"encryptedData":${userData.encryptedData},"iv":${userData.iv}}`
-  console.log(reqdata)
+import wepy from 'wepy' 
+export function kachaAuthApi(loginInfo,userData){ 
+  let reqdata = {
+    'v':'1.0.0',
+    'client':'wx',
+    'command':'1003',
+    'user':{
+      'code':loginInfo.code,
+      'nickName':userData.userInfo.nickName,
+      'headurl':userData.userInfo.avatarUrl,
+      'sex':userData.userInfo.gender,
+      'encryptedData':userData.encryptedData,
+      'iv':userData.iv
+    }
+  } 
   return wepy.request({
     url:'https://user.9kacha.com/wxuser.php',
     method:'POST',
     data:{
-      jparams:reqdata
+      jparams:JSON.stringify(reqdata)
     },
     header:{
-      "Content-Type": "json",
+      "Content-Type": "application/x-www-form-urlencoded" 
     } 
-  }).then((res) => {
-    if(res){
-      return Promise.resolve(res)
+  }).then((res) => { 
+    if(res.statusCode==200 && res.data){
+      let data = res.data
+      if(data.result.accept=="1"){
+        return Promise.resolve(data.user)
+      }else{
+        return Promise.reject('用户登录失败')
+      }
     }else{
-      return Promise.reject('数据获取失败')
-    }
-  }) 
+       return Promise.reject('网络错误')
+    } 
+  })
 }
--- src/common/api/txtSearchApi/index.js
+++ src/common/api/txtSearchApi/index.js
@@ -1,20 +1,94 @@
 import wepy from 'wepy'
-export function hotWordApi(){ 
+const requestHeader = {
+  method:'POST',
+  header:{
+    'content-type':'application/x-www-form-urlencoded;charset=UTF-8'
+  }
+}
+const authObj = {
+  authmode:'user',
+  authparams:{...wepy.getStorageSync('UserInfo').kachaUserInfo}  
+}
+
+// 热门列表接口
+export function hotWordApi(){  
+  let data = { wordStr:'', ...authObj } 
   return wepy.request({
-    url:'https://m.9kacha.com/wsearch/dataApi/get_data.php',
-    method:'POST',
+    url:'https://wdsapi.9kacha.com/util/word_search.php', 
     data:{
       dtype:2,
-      jparams:JSON.stringify({wordStr:''})
-    },
-    header:{
-      'content-type':'application/x-www-form-urlencoded;charset=UTF-8'
-    } 
+      jparams:JSON.stringify(data)
+    }, 
+    ...requestHeader 
   }).then((res) => {
-    if(res){
-      return Promise.resolve(res)
+    if(res && res.statusCode==200){
+      let data = res.data
+      if(!data.status && data.wordHot.length){
+        return Promise.resolve(data.wordHot)
+      }else{
+        return Promise.reject('数据获取失败')
+      }
     }else{
-      return Promise.reject('数据获取失败')
+      return Promise.reject('网络请求失败')
     }
   }) 
+}
+
+
+// 关联选项接口
+export function relateWordApi(queryStr){
+  let data = {wordStr:queryStr, ...authObj}
+  return wepy.request({
+    url:'https://wdsapi.9kacha.com/util/word_suggest.php', 
+    data:{ 
+      jparams:JSON.stringify(data)
+    },
+    ...requestHeader 
+  }).then((res) => { 
+    if(res && res.statusCode==200){
+      let data = res.data
+      if(!data.status ){
+        return Promise.resolve(data.wordOnSuggest)
+      }else{
+        return Promise.reject('数据获取失败')
+      }
+    }else{
+      return Promise.reject('网络请求失败')
+    } 
+  })
+}
+
+export function resultListApi(info){
+  let data = {
+    wordStr:info.wordStr,
+    size:10,
+    fuzzy_flag:1, //关闭模糊搜索
+    offset:info.pageIndex*10-10, 
+    //is_correct:is_correct, // 默认开启 错误匹配
+    //is_alias:is_alias, // 默认开启别名适配
+    //grape_blend_flag:isMix?1:0,  // 默认不是混酿
+    //rowSearch:rowSearch, 
+    ...authObj
+  }
+  return wepy.request({
+    url: 'https://wdsapi.9kacha.com/util/word_search.php',
+    data: {
+      jparams: JSON.stringify(data)
+    },
+    ...requestHeader
+  }).then((res) => {
+    console.log(res)
+    
+    /*if(res && res.statusCode==200){
+      let data = res.data
+      if(!data.status ){
+        return Promise.resolve(data.wordOnSuggest)
+      }else{
+        return Promise.reject('数据获取失败')
+      }
+    }else{
+      return Promise.reject('网络请求失败')
+    }*/
+
+  })
 }
